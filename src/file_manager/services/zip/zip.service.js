ecf9296806b771fd6b18e6abcdf1b63e9a865eff
maria-kashpur <kashpur.maria@icloud.com>
2024-01-25 16:45:42 +0300

    feat: add ZipService

---
+++ src/file_manager/services/zip/zip.service.js
@@ -0,0 +1,9 @@
+export default class ZipService {
+  static compress(pathToFile, pathToDestination) {
+    console.log("compress is success");
+  }
+  
+  static decompress(pathToFile, pathToDestination) {
+    console.log("decompress is success");
+  }
+}
