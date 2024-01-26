e635cea4d91f5eee2158a296822ae414d2813d9b
maria-kashpur <kashpur.maria@icloud.com>
2024-01-26 10:50:01 +0300

    refactor: refactor MessageService

--- src/file_manager/services/massage/message.service.js
+++ src/file_manager/services/massage/message.service.js
@@ -1,21 +1,21 @@
 export default class MessagesService {
-  static greetings(username = "User") {
-    return `Welcome to the File Manager, ${username}!`;
+  static greetings(username) {
+    console.log(`Welcome to the File Manager, ${username}!`)
   }
 
-  static goodbye(username = "User") {
-    return `Thank you for using File Manager, ${username}, goodbye!`;
+  static goodbye(username) {
+    console.log(`Thank you for using File Manager, ${username}, goodbye!`);
   }
 
   static workingDirectory(dir) {
-    return `You are currently in ${dir}.`;
+    console.log(`You are currently in ${dir}.`)
   }
 
   static unknownOperation() {
-    return `Invalid input`;
+    console.log(`Invalid input.`)
   }
 
   static errorExecutionOfOperation() {
-    return `Operation failed`;
+    console.log(`Operation failed.`)
   }
 }
